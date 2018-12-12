db7a9c3ec0cd84e79357ef892c2716aa394e78c3
yewenyu-leaf <1311696342@qq.com>
2018-12-12 15:41:24 +0800

    修改路径

--- Nubia_demo/static/js/photographyTimelate.js
+++ Nubia_demo/static/js/photographyTimelate.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cf70d9854072d29e6772";
+/******/ 	var hotCurrentHash = "63023e0a84436a755e64";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
