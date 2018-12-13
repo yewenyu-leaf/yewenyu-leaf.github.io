c10e15b19d2ad951334d53bbcc25890553c9e319
yewenyu-leaf <1311696342@qq.com>
2018-12-13 09:44:28 +0800

    修改路径

--- Nubia_demo/static/js/x_function.js
+++ Nubia_demo/static/js/x_function.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7b51340dc0f98f4d976b";
+/******/ 	var hotCurrentHash = "b24b412c8fd7c15775f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -782,7 +782,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8081/";
+/******/ 	__webpack_require__.p = "https://yewenyu-leaf.github.io/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
