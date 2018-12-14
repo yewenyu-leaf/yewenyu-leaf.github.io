9f8633da6fcfacf055d5310d49afa1292dba6395
yewenyu-leaf <1311696342@qq.com>
2018-12-14 16:17:33 +0800

    修改路径

--- Nubia_demo/static/js/photography.js
+++ Nubia_demo/static/js/photography.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7b51340dc0f98f4d976b";
+/******/ 	var hotCurrentHash = "673aeb851aa6859193fc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -782,7 +782,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://localhost:8081/";
+/******/ 	__webpack_require__.p = "https://yewenyu-leaf.github.io/Nubia_demo/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
